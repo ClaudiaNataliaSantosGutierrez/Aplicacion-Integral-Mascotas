e29df8e29bc376f8fda6ade4708e2208c9575f53
Claudia Natalia Santos Gutiérrez <claudiasantosgutierrez38@gmail.com>
2021-12-07 12:58:07 -0500

    Creacion de formularios de Administracion, Seguridad, Solicitudes y Visitas, enrutamiento Lazyloading

--- src/app/modulos/solicitudes/solicitudes-routing.module.ts
+++ src/app/modulos/solicitudes/solicitudes-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearSolicitudComponent } from './crear-solicitud/crear-solicitud.component';
+import { EditarSolicitudComponent } from './editar-solicitud/editar-solicitud.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'crear-solicitud',
+    component: CrearSolicitudComponent
+  },
+  {
+    path: 'editar-solicitud',
+    component: EditarSolicitudComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
