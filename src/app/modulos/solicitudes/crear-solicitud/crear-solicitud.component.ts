80aaf0b930997509f79c49e7e12fef49c552a43c
Claudia Natalia Santos Gutiérrez <claudiasantosgutierrez38@gmail.com>
2021-12-08 22:16:57 -0500

    Creacion de CRUD Mascotas y CRUD Solicitudes

--- src/app/modulos/solicitudes/crear-solicitud/crear-solicitud.component.ts
+++ src/app/modulos/solicitudes/crear-solicitud/crear-solicitud.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ModeloMascota } from 'src/app/modelos/mascota.modelo';
+import { ModeloPersona } from 'src/app/modelos/persona.modelo';
+import { ModeloSolicitud } from 'src/app/modelos/solicitud.modelo';
+import { ModeloVeterinario } from 'src/app/modelos/veterinario.modelo';
+import { MascotaService } from 'src/app/servicios/mascota.service';
+import { PersonaService } from 'src/app/servicios/persona.service';
+import { SolicitudService } from 'src/app/servicios/solicitud.service';
+import { VeterinarioService } from 'src/app/servicios/veterinario.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-crear-solicitud',
@@ -7,9 +18,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearSolicitudComponent implements OnInit {
 
-  constructor() { }
+  //Objetos para traer la lista de personas y la cantidad de personas
+  listaMascotas: ModeloMascota[] = [];
+  listaPersonas: ModeloPersona[] = [];
+  listaVeterinarios: ModeloVeterinario[] = [];
+
+  fgValidador: FormGroup = this.fb.group({
+    'fecha': ['', [Validators.required]],
+    'detalle': ['', [Validators.required]],
+    'personaId': ['', [Validators.required]],
+    'veterinarioId': ['', [Validators.required]],
+    'mascotaId': ['', [Validators.required]]
+  });
+
+  constructor(private fb: FormBuilder,
+    private solicitudServicio: SolicitudService,
+    private mascotaServicio: MascotaService,
+    private personaServicio: PersonaService,
+    private veterinarioServicio: VeterinarioService,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.ObtenerListadoPersonas();
+    this.ObtenerListadoVeterinarios();
+    this.ObtenerListadoMascotas();
+  }
+
+  GuardarSolicitud(){
+    let fecha = this.fgValidador.controls['fecha'].value;
+    //Conversion de Fecha para que quede en formato MongoDB ISO8601
+    var fecha1 = new Date(fecha);
+    var fecha2 = moment(fecha1);
+    var fecha3 = fecha2.toISOString();
+    let detalle = this.fgValidador.controls['detalle'].value;
+    let personaId = this.fgValidador.controls['personaId'].value;
+    let veterinarioId = this.fgValidador.controls['veterinarioId'].value;
+    let mascotaId = this.fgValidador.controls['mascotaId'].value;
+    let s = new ModeloSolicitud();
+    s.fecha = fecha3;
+    s.detalle = detalle;
+    s.personaId = personaId;
+    s.veterinarioId = veterinarioId;
+    s.mascotaId = mascotaId;
+    this.solicitudServicio.CrearSolicitud(s).subscribe((datos: ModeloSolicitud) => {
+      alert("Solicitud de Atención creada con exito");
+      this.router.navigate(['/solicitudes/listar-solicitudes']);
+    }, (error: any) => {
+      alert("Error al crear la nueva Solicitud");
+    })
+
+  }
+
+  //Trae la lista de personas
+  ObtenerListadoPersonas(){
+    this.personaServicio.ObtenerPersonas().subscribe((datos: ModeloPersona[]) => {
+      this.listaPersonas = datos;
+    })
+  }
+
+  //Metodo para traer la lista de veterinarios
+  ObtenerListadoVeterinarios(){
+    this.veterinarioServicio.ObtenerVeterinarios().subscribe((datos: ModeloVeterinario[]) => {
+      this.listaVeterinarios = datos;
+    })
+  }
+
+  //Metodo para traer la lista de mascota
+  ObtenerListadoMascotas(){
+    this.mascotaServicio.ObtenerMascotas().subscribe((datos: ModeloMascota[]) => {
+      this.listaMascotas = datos;
+    })
   }
 
 }
--- src/app/modulos/solicitudes/solicitudes-routing.module.ts
+++ src/app/modulos/solicitudes/solicitudes-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BuscarSolicitudComponent } from './buscar-solicitud/buscar-solicitud.component';
 import { CrearSolicitudComponent } from './crear-solicitud/crear-solicitud.component';
 import { EditarSolicitudComponent } from './editar-solicitud/editar-solicitud.component';
+import { EliminarSolicitudComponent } from './eliminar-solicitud/eliminar-solicitud.component';
 
 const routes: Routes = [
   {
@@ -9,9 +11,21 @@ const routes: Routes = [
     component: CrearSolicitudComponent
   },
   {
-    path: 'editar-solicitud',
+    path: 'editar-solicitud/:id',
     component: EditarSolicitudComponent
-  }
+  },
+  {
+    path: 'eliminar-solicitud/:id',
+    component: EliminarSolicitudComponent
+  },
+  {
+    path: 'buscar-solicitud',
+    component: BuscarSolicitudComponent
+  },
+  {
+    path: 'listar-solicitudes',
+    component: BuscarSolicitudComponent
+  },
 ];
 
 @NgModule({
