9edc0cb3377d913de0cb4d715a415ebb1fb50588
Claudia Natalia Santos Gutiérrez <claudiasantosgutierrez38@gmail.com>
2021-12-10 08:37:31 -0500

    Guardianes

--- src/app/modulos/visita/visita-routing.module.ts
+++ src/app/modulos/visita/visita-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ValidadorSesionGuard } from 'src/app/guardianes/validador-sesion.guard';
 import { BuscarVisitaComponent } from './buscar-visita/buscar-visita.component';
 import { EditarVisitaComponent } from './editar-visita/editar-visita.component';
 import { EliminarVisitaComponent } from './eliminar-visita/eliminar-visita.component';
@@ -9,22 +10,27 @@ const routes: Routes = [
   {
     path: 'hacer-visita',
     component : HacerVisitaComponent,
+    canActivate: [ValidadorSesionGuard]
   },
   {
     path: 'editar-visita/:id',
     component : EditarVisitaComponent,
+    canActivate: [ValidadorSesionGuard]
   },
   {
     path: 'eliminar-visita/:id',
     component : EliminarVisitaComponent,
+    canActivate: [ValidadorSesionGuard]
   },
   {
     path: 'buscar-visita',
     component : BuscarVisitaComponent,
+    canActivate: [ValidadorSesionGuard]
   },
   {
     path: 'listar-visitas',
     component : BuscarVisitaComponent,
+    canActivate: [ValidadorSesionGuard]
   }
 ];
 
